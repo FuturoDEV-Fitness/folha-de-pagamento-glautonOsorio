f0be40542a6175b315e32e22bcb88115d831a537
Glauton Osório <glautonleao@gmail.com>
2024-05-19 21:36:33 -0300

    feat: adicionando calculo inss

---
+++ src/calc_inss.js
@@ -0,0 +1,20 @@
+function calculateInss(grossSalary) {
+  const INSS_CEILING = 908.85;
+  let inss = 0;
+
+  if (grossSalary <= 1412) {
+    inss = grossSalary * (7.5 / 100);
+  } else if (grossSalary >= 1412.01 && grossSalary <= 2666.68) {
+    inss = grossSalary * (9 / 100);
+  } else if (grossSalary >= 2666.69 && grossSalary <= 4000.03) {
+    inss = grossSalary * (12 / 100);
+  } else {
+    inss = grossSalary * (14 / 100);
+  }
+
+  if (inss >= INSS_CEILING) inss = INSS_CEILING;
+
+  return inss;
+}
+
+module.exports = calculateInss;
