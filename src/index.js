601b8c63f30c82ffe53a4a37eee26a56910e13c7
Glauton Osório <glautonleao@gmail.com>
2024-05-21 17:47:27 -0300

    perguntas do funcionario completadas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Amigo estou aqui");
+const read = require("readline-sync");
+const Calculate = require("./calculate");
+
+const nome = read.question("Qual o nome do funcionário?");
+const cpf = read.question("Qual o cpf do funcionário?");
+const salario = Number(
+  read.question("Qual o salario bruto do funcionário?").replace(",", ".")
+);
+
+const inss = Calculate.inss(salario);
+const tax = Calculate.tax(salario);
+const salarioLiquido = Calculate.liquid(salario);
+
+console.log(`Nome: ${nome}`);
+console.log(`CPF: ${cpf}`);
+console.log(`Salário Bruto: R$${salario.toFixed()}`);
+console.log(`INSS: R$${inss.toFixed()}`);
+console.log(`Imposto: R$${tax.toFixed()}`);
+console.log(`Salário Líquido: R$${salarioLiquido.toFixed()}`);
